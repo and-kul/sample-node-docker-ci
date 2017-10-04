7bc2dc05a9c18d7c64e39ba7436512167f7953b8
Andrey Kulagin <mr.and.kul@gmail.com>
2017-10-04 21:37:58 +0300

    zzz

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ docker stop web || true
 docker rm web || true
 docker rmi andkul/sample-node:current || true
 docker tag andkul/sample-node:latest andkul/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 andkul/sample-node:current
+docker run -d --restart always --name web -p 80:80 andkul/sample-node:current
 EOF
